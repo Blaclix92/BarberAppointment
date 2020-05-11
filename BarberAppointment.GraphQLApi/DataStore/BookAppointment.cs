10daaa4cdea30d17d9496835498dde91f2380490
Benny <bennymohan08@gmail.com>
2020-05-12 01:08:51 +0200

    Add new Api with GraphQL

---
+++ BarberAppointment.GraphQLApi/DataStore/BookAppointment.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberAppointment.GraphQLApi.DataStore
+{
+    public class BookAppointment
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public int Cancel { get; set; }
+        public string Hour { get; set; }
+        public int BarberId { get; set; }
+        public int ClientId { get; set; }
+    }
+}
---
+++ BarberAppointment.GraphQLApi/DataStore/BookAppointmentsDataStore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarberAppointment.GraphQLApi.DataStore
+{
+    public class BookAppointmentsDataStore
+    {
+        public static BookAppointmentsDataStore Current { get; } = new BookAppointmentsDataStore();
+        public List<BookAppointment> Appointments { get; set; }
+        public BookAppointmentsDataStore()
+        {
+            //init dummy data
+            Appointments = new List<BookAppointment>()
+            {
+                new BookAppointment()
+                {
+                    Id = 1,
+                    Date = "02-04-2019",
+                    Hour = "11:00 AM",
+                    Cancel= 0,
+                    BarberId = 2,
+                    ClientId = 3
+                },
+                  new BookAppointment()
+                {
+                    Id = 2,
+                    Date = "10-04-2019",
+                    Hour = "11:00 AM",
+                    Cancel= 0,
+                    BarberId = 2,
+                    ClientId = 3
+                },  new BookAppointment()
+                {
+                    Id = 3,
+                    Date = "25-04-2019",
+                    Hour = "11:00 AM",
+                    Cancel= 0,
+                    BarberId = 2,
+                    ClientId = 3
+                },
+            };
+        }
+    }
+}
