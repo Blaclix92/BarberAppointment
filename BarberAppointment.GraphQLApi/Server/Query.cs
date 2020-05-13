e26992e19ddd60fc3292ab4dbb8b8e37e4f3ce58
Benny <bennymohan08@gmail.com>
2020-05-13 16:36:50 +0200

    add query to get bookappointments by date

--- BarberAppointment.GraphQLApi/Server/Query.cs
+++ BarberAppointment.GraphQLApi/Server/Query.cs
@@ -12,6 +12,7 @@ namespace BarberAppointment.GraphQLApi.Server
     public class Query
     {
         public Dictionary<string, BookAppointment> BookAppointments() => new BookAppointmentRepository().getData();
+        public List<BookAppointment> BookAppointmentsByDate(string date) => new BookAppointmentRepository().getData().Values.Where(p => p.Date == date).ToList();
         public Dictionary<string, WorkDay> WorkDays() => new WorkDayRepository().getData();
         public Dictionary<string, CustomWorkDay> CustomWorkDay() => new CustomWorkDayRepository().getData();
 
